94ba26c91c8bd09a8fe72369749f505ff0df618f
Namrata Kedar <110167648+Namrataaissms@users.noreply.github.com>
2022-07-31 21:25:22 +0530

    Add files via upload

---
+++ Star2.java
@@ -0,0 +1,13 @@
+package Star_pattern;
+
+public class Star2 {
+    public static void main(String args[]) {
+        for (int i = 0; i < 5; i++) {
+            for (int j=5;j>i;j--){
+                System.out.print(" * ");
+            }
+            System.out.println("  ");
+
+        }
+    }
+}
