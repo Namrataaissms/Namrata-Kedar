4ce5b0530ae392989b6d7eea9dfeec7bc9ec8a5e
Namrata Kedar <110167648+Namrataaissms@users.noreply.github.com>
2022-07-31 21:26:28 +0530

    Add files via upload

---
+++ Star5.java
@@ -0,0 +1,15 @@
+package Star_pattern;
+
+public class Star5 {
+    public static void main(String args[]){
+        for(int i=5;i>0;i--){
+            for(int j=5;j>i;j--){
+                System.out.print("  ");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print("  * ");
+            }
+            System.out.println();
+        }
+    }
+}
